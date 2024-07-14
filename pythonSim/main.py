8913353aeaa4de0c44be9d59f9ee7693b4e64173
lightfrantice <lightfrantice@gmail.com>
2024-07-14 18:29:29 +0900

    move conversion functions to the convertion module, because of the tensorflow does not work on my PC, i just removed tf32 and use fp64 to instead

--- pythonSim/main.py
+++ pythonSim/main.py
@@ -2,63 +2,81 @@ import argparse
 
 from fpModule import fpModule
 from multsByPrecisions import FPMult
+from convertion import bitStrToNum, numToBitstr
 
-class TranslateToBinary:
-    def __init__(self) -> None:
-        pass
-
-    def dataStrToNum(dataStr, precision):
-        if 'fp' in precision:
-            dataList = dataStr.split('.')
-            intPart = dataList[0]
-            decimalPart = dataList[1]
-           
-            if precision == 'fp32':
-                pass
-            
-
-    def toBinary(dataStr, precision):
-        pass
-          
 
 def main():
 
     parser = argparse.ArgumentParser(description='Set precision and calculate mode.')
     
-    parser.add_argument('-p', '--precision', choices=['fp32', 'fp16', 'fp8', 'bf16', 'int8', 'int16', 'int32', 'tf32'], help='Choose a precision', required=True)
+    parser.add_argument('-p', '--precision', choices=['fp64', 'fp32', 'fp16', 'fp8', 'bf16', 'int8', 'int16', 'int32'], help='Choose a precision', required=True)
     parser.add_argument('-c', '--calmode', choices=['add', 'mult'], help='Choose a calculating mode, add or mult.', required=True)
     
-    parser.add_argument('dataStr0', help='The first data to be calculated.')
-    parser.add_argument('dataStr1', help='The second data to be calculated.')
+    parser.add_argument('data0', help='The first data to be calculated.')
+    parser.add_argument('data1', help='The second data to be calculated.')
 
     args = parser.parse_args()
 
     #test args
-    print(args)
+    #print(args)
 
     mode = args.calmode
 
     precision = args.precision
 
-    dataStr0 = args.dataStr0
-    dataStr1 = args.dataStr1
+    dataStr0 = numToBitstr(args.data0, precision)
+    dataStr1 = numToBitstr(args.data1, precision)
 
     result = ''
 
-    if precision == 'fp32':
-            if mode == "add":
-                result = fpModule(dataStr0, dataStr1, precision).fp32Adder
-            elif mode == "mult":
-                result = FPMult.fp32(dataStr0, dataStr1)
-            else:
-                raise ValueError(f"Unsupported calculating type: {mode}")
-        
+    if mode == 'add':
+
+        if precision == 'fp64' or precision == 'fp32' or precision == 'fp16' or precision == 'fp8' or precision == 'bf16':
+            result = fpModule(dataStr0, dataStr1, precision).adder()
+
+        else:
+            raise TypeError(f"Unsupported precision type: {precision}")
+
+    elif mode == "mult":
+
+        if precision == 'fp64' or precision == 'fp32' or precision == 'fp16' or precision == 'fp8' or precision == 'bf16':
+            result = fpModule(dataStr0, dataStr1, precision).mult()
+
+        else:
+            raise TypeError(f"Unsupported precision type: {precision}")
     else:
-        raise ValueError(f"Unsupported precision type: {precision}")
+        raise TypeError(f"Unsupported calculating type: {mode}")
+        
     
-    return result
+    
+    deResult = bitStrToNum(result, precision)
+
+    return args.data0, args.data1, result, deResult, precision
 
 
 if __name__ == "__main__" :
-    result = main()
-    print(result)
+    #print(numToBitstr(3.12, 'fp32'))
+    
+    num0, num1, result, deResult, precision = main()
+
+    if precision == 'fp64':
+        roundUp = '.14e'
+    elif precision == 'fp32':
+        roundUp = '.6e'
+    elif precision == 'fp16':
+        roundUp = '.2e'
+    elif precision == 'fp8':
+        roundUp = '.0e'
+    elif precision == 'bf16':
+        roundUp = '.1e'
+
+    print(f'Binary Result: {result}')
+    print(f'Decimal Result: {format(deResult, roundUp)}')
+
+    
+
+    print(f'True Result: {format(float(num0)*float(num1), roundUp)}')
+    if format(deResult, roundUp) == format(float(num0)*float(num1), roundUp):
+        print(True)
+    else:
+        print(False)

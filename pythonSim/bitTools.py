fd9f122a097bfc2b5f002203b1e0ada7411e698b
lightfrantice <lightfrantice@gmail.com>
2024-07-14 18:25:00 +0900

    use nBitRCA instead of BCA, in function binary sort, return a new string instead of return the inputted strings, add subWithAustrainMethod function but not be used

--- pythonSim/bitTools.py
+++ pythonSim/bitTools.py
@@ -1,21 +1,27 @@
 from logicGates import LogicGates
-from nBitCA import nBitBCA
+from nBitCA import nBitRCA
 
 def binarySort(binaryStr0, binaryStr1):
+    newBinaryStr0 = binaryStr0
+    newBinaryStr1 = binaryStr1
     for i in range(len(binaryStr0)):
-        if binaryStr0[i] == '1' and binaryStr1[i] == '0':
-            return binaryStr0, binaryStr1, '0'
-        elif binaryStr0[i] == '1' and binaryStr1[i] == '0':
-            return binaryStr1, binaryStr0, '1'
+        if binaryStr0[i] == '1' and binaryStr1[i] == '0':     
+            return newBinaryStr0, newBinaryStr1, '0'
+        elif binaryStr1[i] == '1' and binaryStr0[i] == '0':
+            return newBinaryStr1, newBinaryStr0, '1'
         else:
             pass
-    return binaryStr0, binaryStr1
+    return newBinaryStr0, newBinaryStr1, '0'
 
+def getGap(str0, str1):
+    bigger, smaller, p = binarySort(str0, str1)
+    smaller = getMinus(smaller)
+    gap, throwedCarry = nBitRCA(bigger, smaller)
+    return gap, p
 
 def getMinus(mantissaStr):
     reversedMantissaStr = ''
     just1 = ''
-
     bitLen = len(mantissaStr)
 
     for bit in mantissaStr:
@@ -26,7 +32,7 @@ def getMinus(mantissaStr):
         just1 = just1 + '0'
     just1 = just1 + '1'
 
-    sumStr, carry = nBitBCA(reversedMantissaStr, just1)
+    sumStr, carry = nBitRCA(reversedMantissaStr, just1)
 
     return sumStr
 
@@ -35,12 +41,12 @@ def leftShift(dataStr, shiftBitStr):
     newDataStr = dataStr
     lenShiftStr = len(shiftBitStr)
     for i in range(lenShiftStr):
-        print(f'position:{i}')
+        #print(f'position:{i}')
         if shiftBitStr[lenShiftStr-1-i]=='1':
             addZeroToEnd = newDataStr + '0'*(2**i)
-            print(f'addZero: {addZeroToEnd}')
+            #print(f'addZero: {addZeroToEnd}')
             newDataStr = addZeroToEnd[2**i:]
-            print(f'new:{newDataStr}')
+            #print(f'new:{newDataStr}')
     return newDataStr
 
 def rightShift(dataStr, shiftBitStr):
@@ -48,16 +54,46 @@ def rightShift(dataStr, shiftBitStr):
     newDataStr = dataStr
     lenShiftStr = len(shiftBitStr)
     for i in range(lenShiftStr):
-        print(f'position:{i}')
+        #print(f'position:{i}')
         if shiftBitStr[lenShiftStr-1-i]=='1':
             addZeroToEnd = '0'*(2**i) + newDataStr
-            print(f'addZero: {addZeroToEnd}')
+            #print(f'addZero: {addZeroToEnd}')
             newDataStr = addZeroToEnd[0:length]
-            print(f'new:{newDataStr}')
+            #print(f'new:{newDataStr}')
     return newDataStr
 
+def subWithAustrainMethod(strGreater, strSmaller):
+    dataLen = len(strGreater)
+    austrian = '0'
+    result = ''
+    for i in range(dataLen-1, -1, -1):
+        if strGreater[i] == '1' and strSmaller[i] == '0' and austrian == '0':
+            result = '1' + result
+        elif strGreater[i] == '1' and strSmaller[i] == '0' and austrian == '1':
+            result = '0' + result
+        elif strGreater[i] == '1' and strSmaller[i] == '1' and austrian == '0':
+            result = '0' + result
+        elif strGreater[i] == '1' and strSmaller[i] == '1' and austrian == '1':
+            result = '1' + result
+            austrian = '1'
+        elif strGreater[i] == '0' and strSmaller[i] == '0' and austrian == '0':
+            result = '0' + result
+        elif strGreater[i] == '0' and strSmaller[i] == '0' and austrian == '1':
+            result = '1' + result
+            austrian = '1'
+        elif strGreater[i] == '0' and strSmaller[i] == '1' and austrian == '0':
+            result = '1' + result
+            austrian = '1'
+        elif strGreater[i] == '0' and strSmaller[i] == '1' and austrian == '1':
+            result = '0' + result
+            austrian = '1'
+    return result
+
+
+
+
 def main():
-    a = rightShift('1010100', '0010')
+    a = subWithAustrainMethod('1001', '0010')
     print(a)
 
 if __name__ == "__main__":
